423f3de2f71431c04daec6aec09309557edc00f9
Emre Kurt <emre.kurt1099@gmail.com>
2024-01-04 22:03:33 +0300

    Kucuk CSS Duzeltmeleri

--- src/List.jsx
+++ src/List.jsx
@@ -301,10 +301,10 @@ export default function List() {
                         <svg className="checkBox-hover themeBlue" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" focusable="false"><path d="M10 2a8 8 0 110 16 8 8 0 010-16zm0 1a7 7 0 100 14 7 7 0 000-14zm3.36 4.65c.17.17.2.44.06.63l-.06.07-4 4a.5.5 0 01-.64.07l-.07-.06-2-2a.5.5 0 01.63-.77l.07.06L9 11.3l3.65-3.65c.2-.2.51-.2.7 0z" fill="currentColor"></path></svg>
                     </span>
                     <ul>
-                        <li className="baseAddInput-important">
+                        <li className={`baseAddInput-important ${editButton ? 'editMode' : ''}`}>
                             <div className="whatTodo">{!editButton ? task.todo : 
                                 <form onSubmit={ (e) => sendNewTodo(e, task.id)}>
-                                    <input className="baseAddInput-important" type="text" maxLength={"255"} placeholder="Add a task" tabIndex={"0"} autoComplete="off" name="editedTodo" disabled={!userID} defaultValue={task.todo}/>
+                                    <input className="baseAddInput-important editTodo" type="text" maxLength={"255"} placeholder="Add a task" tabIndex={"0"} autoComplete="off" name="editedTodo" disabled={!userID} defaultValue={task.todo}/>
                                 </form>}
                             </div>  
                                 <div className="impRepChecker"> 
